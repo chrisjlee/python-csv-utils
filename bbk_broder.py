e2ebd49c5b284ac5fdca7ccc0b9daaf6867c3f52
Chris Lee <chris@globerunnerseo.com>
2012-04-02 17:14:43 -0500

    [CL] Nightly Commit

--- bbk_broder.py
+++ bbk_broder.py
@@ -1,7 +1,16 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 #from collections import defaultdict
 import csv
 
+"""
+Merge the last three csv items and pipe them
+@param row:
+current row taken in from csv iterator
+"""
+def mergecat(row):
+    return row[:-3] + ['|'.join(row[-3:])]
+def decodePrices(row):
+    return row[4].split(';')
 cr = csv.reader(open('test.csv', 'rb'))
 header = cr.next()
 co = csv.writer(open('test2.csv','wb'))
@@ -12,9 +21,11 @@ for row in cr:
         index = row[2].index(' - ')
         if index > 0:
             # print row[2][0:index], 
-            row[2] = row[2][0:index]
-            print row[2]
+            row.append(row[2][0:index])
+            #print row[2]
     except:
         pass
-    co.writerow(row)
+    row = mergecat(row)
     print row
+    if not row[0].startswith('SKU'):
+      co.writerow(row)
