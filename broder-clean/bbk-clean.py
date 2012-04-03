14b83befc62b49081ec524ff9496b8a399056c4f
Chris Lee <chris@globerunnerseo.com>
2012-04-03 13:52:11 -0500

    [CL] Tornado commit! Woot woot

--- broder-clean/bbk-clean.py
+++ broder-clean/bbk-clean.py
@@ -1,18 +1,39 @@
 #!/usr/bin/env python
 import csv
+import re
 import os
 
 """
 Clean the broder files and remove nasty characters
 """
 
-class bbk: pass
-
-def parse(fn, op):
-    cr = csv.reader(open(fn, 'rb'))
-    header = cr.next()
-    co = csv.writer(open(op,'wb'))
-    return cr, co
-bbk.cr = parse('update.csv','output.csv')
-for row in bbk.cr enumerate:
-    print row
+with open("output.csv", "wt") as out:
+    for l in open("update.csv", 'r'):
+        l = l.replace('^', ',', -1)
+        print l
+        out.write(l)
+#def parse(fn, op):
+#    cr = csv.reader(open(fn, 'rb'))
+#    header = cr.next()
+#    co = csv.writer(open(op,'wb'))
+#    return cr, co
+#"""
+#  Replace seperated values
+#      @param row: csv row
+#      @param seperator: replaced text value to convert seperator 
+#"""
+#def seperate_values(row,seperator):
+#    return row.replace(seperator,',')
+## Return parsed values
+###cr = parse('update.csv','output.csv')
+#print co
+#header = cr.next()
+#
+#for i, row in enumerate(cr):
+#    print row
+#    seperate_values(row,'^')
+    
+#def main():
+#    return null
+#if __name__ == '__main__':
+#    main()
