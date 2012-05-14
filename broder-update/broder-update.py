a966a7d46de2c4d04e43d1e65080d58be846cc0e
Chris Lee <chris@globerunnerseo.com>
2012-05-14 17:25:35 -0500

    [CL] Nightly commit

--- broder-update/broder-update.py
+++ broder-update/broder-update.py
@@ -12,12 +12,14 @@ import sys
 import itertools
 import argparse
 
+#
+# Usage Example: ./broder-update.py -u  update.csv -i broder-entities.csv -o output.csv
+#
+
 # Derive the name of the CSV file from the name of the script and initialise
 # the headers list and content dictionary
 def parse_arguments():
-    """
-    Function manages arguments
-    """
+    """ Function manages arguments """
     parser = argparse.ArgumentParser(prog='Price Formatter ', description=__doc__, epilog='Author: Chris Lee (chris@globerunnerseo.com)')
     parser.add_argument('-i', '--input', help='Input file', nargs='?', type=argparse.FileType('r'), default=sys.stdin, required=True)
     parser.add_argument('-u', '--update', help='Update file', nargs='?', type=argparse.FileType('r'), default=sys.stdin, required=True)
@@ -28,58 +30,37 @@ def parse_arguments():
     argsdict = vars(args)
     # Return each argument 
     return argsdict['input'], argsdict['update'], argsdict['output']
+#def print_header(dh,Dy):
+#    for i,z in (zip (dh,Dy)):
+#        print 'input: %s \t update: %s' % (i,z)
 def csv_to_dict(file):
-    d = csv.DictReader(file, dialect='excel', delimiter=',')
-    #for row in d:
-        #for key in row:
-            # d = key, row[key]
-            # print 'key=%s, value=%s' % (key, row[key])
-            # print d[key]
-    return d
-#reader=csv.reader(open(fi))
-#for row in reader:
-#    if reader.line_num == 1:
-#        """
-#        If we are on the first line, create the headers list from the first row
-#        by taking a slice from item 1  as we don't need the very first header.
-#        """
-#        headers = row[1:]
-#    else:
-#        """
-#        Otherwise, the key in the content dictionary is the first item in the
-#        row and we can create the sub-dictionary by using the zip() function.
-#        We also know that the stabling entry is a comma separated list of names
-#        so we split it into a list for easier processing.
-#        """
-#        content[row[0]] = dict(zip(headers, row[1:]))
-#        content[row[0]]['Stabling'] = [s.strip() for s in content[row[0]]['Stabling'].split(',')]
-#
-## We can know get to the content by using the resulting dictionary, so to see
-## the list of lines, we can do:
-#print "\nList of lines"
-#print content.keys()
-## To see the list of statistics available for each line
-#print "\nAvailable statistics for each line"
-#print headers
-#
-#header = ["SKU","Status","Title","Price","Weight","Company","Style Code","Features","Retail Price","Size","Hex Color","Color","Mill Name","Dimensions","Imprint Area","Item Number","Image","Category"]
-#
-#
-def csv_to_dict(file):
+    """ take the file (csv reader) """
     return csv.DictReader(file, dialect='excel', delimiter=',')
-"""
-    main function
+""" main function
 -------------------------------------------------------------------------"""
 if __name__ == '__main__':
+    import doctest
+    
+    # Create a list for matching header columns
+    mc = [] 
     # Grab the input / output path
     input, update, output = parse_arguments()
-    args = [input, update] 
+    # create a dictionary object from the input
     d = csv_to_dict(input)
+    # create a dict from an  update file
     u = csv_to_dict(update)
-    allrows = list(csv.reader(update))
-    # Extract the first row as keys for a columns dictionary
-    cols = dict([(x[0],x[1:]) for x in zip(*allrows)])
-    print cols
-#    for key in d:
-#        print l['NEW']
-#        if l['NEW'] == 'NEW': 
+    #d = csv.DictReader(input, dialect='excel', delimiter=',') #csv_to_dict(input)
+    #u = csv.DictReader(update, dialect='excel', delimiter=',') # csv_to_dict(update)
+    dh = [f for f in d.fieldnames]
+    uh = [f for f in u.fieldnames]
+    """ """
+    matches = []
+    # Take one list and loop through it
+    for i in dh:
+        # Now loop through the other list - the second list should be shorter
+        # @todo: detect which list is the shorter one
+        for k in uh:
+            # Compare
+            if k.lower() == i.lower():
+                matches.append(k)
+    print matches
---
+++ broder-update/test.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./broder-update.py -u update.csv -i broder-entities.csv -o output.csv
